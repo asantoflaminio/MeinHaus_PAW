bf9fe579f0d9c3c10f848b96bfe92075c8a906d5
arodriguez1415 <arodriguez@itba.edu.ar>
2018-08-22 20:23:40 -0300

    Add files via upload

---
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.itba.paw.interfaces.UserService;
+
+@Controller
+@RequestMapping("/hello/")
+public class HelloWorldController {
+	
+	 @Autowired
+	 @Qualifier("userServiceImpl")
+	 private UserService us;
+	
+	@RequestMapping("world")
+	public ModelAndView helloWorld() {
+		final ModelAndView mav = new ModelAndView("index");
+		mav.addObject("greeting", "PAW");
+		return mav;
+	}
+	
+	@RequestMapping("mars")
+	public ModelAndView helloMars() {
+		final ModelAndView mav = new ModelAndView("index");
+		mav.addObject("greeting", "MARS");
+		return mav;
+	}	
+}
