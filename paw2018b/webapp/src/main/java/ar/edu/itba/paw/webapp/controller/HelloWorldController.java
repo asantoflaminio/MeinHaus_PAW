796a6da2f595c71f0b153b0fb1b4a27f46e8712e
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-09-24 11:37:00 -0300

    Added import to avoid error

--- paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw2018b/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
